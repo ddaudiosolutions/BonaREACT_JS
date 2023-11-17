e6386b45083f631cdad91802117b023196330cbb
David_Cladera <dcladera@externos.itnow.es>
2023-11-17 09:02:31 +0100

    change wordpress route

--- App/src/config/axios.js
+++ App/src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-  /* baseURL: "https://www.dermathosmallorca.com/wp-json/wp/v2/posts", */
-  baseURL: "https://www.produciendomimusica.com/wp-json/wp/v2/posts",
+  baseURL: "https://www.dermathosmallorca.com/wp-json/wp/v2/posts",
+  /* baseURL: "https://www.produciendomimusica.com/wp-json/wp/v2/posts", */
 });
 
 export default clienteAxios;
